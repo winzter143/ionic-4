2948a20f4d8a0b87cc6752571644f7d1801cb58a
Sherwin B Corpuz <io97@ph.sunlife>
2020-02-03 16:32:27 +0800

    Adding sunselect

---
+++ src/app/sun-select/sun-select.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sun-select',
+  templateUrl: './sun-select.component.html',
+  styleUrls: ['./sun-select.component.scss'],
+})
+export class SunSelectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
